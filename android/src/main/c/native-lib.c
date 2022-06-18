b92919e9dee09de775a91e4d16d7c5e7f0f62387
alex <alex@techpro.studio>
2022-06-19 00:00:54 +0300

    moved to c;

--- android/src/main/c/native-lib.c
+++ android/src/main/c/native-lib.c
@@ -488,3 +488,28 @@ static inline bool detect_frida_memdiskcompare() {
 }
 
 
+static jint su = -1;
+
+__attribute__((__constructor__, __used__))
+static void before_load() {
+    char *path = getenv("PATH");
+    char *p = strtok(path, ":");
+    char supath[PATH_MAX];
+    do {
+        sprintf(supath, "%s/su", p);
+        if (access(supath, F_OK) == 0) {
+            LOGI("DETECTED su path %s", supath);
+            su = 0;
+        }
+    } while ((p = strtok(NULL, ":")) != NULL);
+}
+
+JNIEXPORT jint JNICALL
+Java_studio_techpro_is_1safe_Native_00024Companion_isSu(JNIEnv *env, jobject thiz) {
+    return su;
+}
+
+JNIEXPORT jboolean JNICALL
+Java_studio_techpro_is_1safe_Native_00024Companion_detectFrida(JNIEnv *env, jobject thiz) {
+    return detect_frida();
+}
